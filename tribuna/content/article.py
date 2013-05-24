1769d6187c60010c54f6e1a4afb05e30daccc042
Termitnjak d.o.o <info@termitnjak.si>
2013-05-24 13:39:32 +0000

    Added a dinamic MultiSelect list for tags in article. Also cleaned the code up a bit, changed code in tags to work with my code after merge, made sure code adheres to pep8 standards

--- tribuna/content/article.py
+++ tribuna/content/article.py
@@ -1,31 +1,57 @@
 from five import grok
-from zope import schema
-
+from plone import api
 from plone.directives import form
-
 from plone.app.textfield import RichText
 from plone.namedfile.field import NamedImage
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary
 
 from tribuna.content import _
+
+
+class TagsList(object):
+    grok.implements(IContextSourceBinder)
+
+    def __init__(self):
+        pass
+
+    def __call__(self, context):
+        catalog = api.portal.get_tool(name='portal_catalog')
+        items = catalog({
+            'portal_type': 'tribuna.content.tag',
+            'review_state': 'published',
+        })
+
+        terms = []
+
+        for item in items:
+            term = item.Title
+            terms.append(SimpleVocabulary.createTerm(term, term, term))
+
+        return SimpleVocabulary(terms)
+
 
 class IArticle(form.Schema):
     """Interface for Article content type
     """
 
     title = schema.TextLine(
-            title=_(u"Name"),
-        )
+        title=_(u"Name"),
+    )
 
     description = schema.Text(
-            title=_(u"Article text"),
-        )
+        title=_(u"Article text"),
+    )
 
     picture = NamedImage(
-            title=_(u"Article image"),
-            description=_(u"Upload an image for your article"),
-            required=False,
-        )
+        title=_(u"Article image"),
+        description=_(u"Upload an image for your article"),
+        required=False,
+    )
 
-    tag = schema.TextLine(
-            title=_(u"Tag name"),
-        )
+    tags = schema.List(
+        title=u"Tags",
+        # source=CountriesList(),
+        value_type=schema.Choice(source=TagsList()),
+    )
--- tribuna/content/tag.py
+++ tribuna/content/tag.py
@@ -1,16 +1,11 @@
 from five import grok
-from zope import schema
-from Acquisition import aq_inner
-from Products.CMFCore.utils import getToolByName
-
 from plone import api
-from plone.directives import form
 from plone.app.textfield import RichText
+from plone.directives import form
 from plone.namedfile.field import NamedImage
+from zope import schema
 
 from tribuna.content import _
-
-from tribuna.content.article import IArticle
 
 
 class ITag(form.Schema):
@@ -18,18 +13,18 @@ class ITag(form.Schema):
     """
 
     title = schema.TextLine(
-            title=_(u"Name"),
-        )
+        title=_(u"Name"),
+    )
 
     description = schema.Text(
-            title=_(u"A short summary"),
-        )
+        title=_(u"A short summary"),
+    )
 
     picture = NamedImage(
-            title=_(u"Picture"),
-            description=_(u"Please upload an image"),
-            required=False,
-        )
+        title=_(u"Picture"),
+        description=_(u"Please upload an image"),
+        required=False,
+    )
 
 
 class View(grok.View):
@@ -40,8 +35,8 @@ class View(grok.View):
         """Return a catalog search result of articles that have this tag
         """
 
-        #context = aq_inner(self.context)
         catalog = api.portal.get_tool(name='portal_catalog')
-        import pdb; pdb.set_trace()
         all_articles = catalog(portal_type="tribuna.content.article")
-        return [article for article in all_articles if article.getObject().tag == self.context.Title()]
+        return [article for article in all_articles
+                if article.review_state == 'published' and
+                self.context.Title() in article.getObject().tags]
