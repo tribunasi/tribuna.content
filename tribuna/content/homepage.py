0adec277b666728784950fe69f04d770539d5e90
Termitnjak d.o.o <info@termitnjak.si>
2013-07-08 12:46:43 +0000

    Added tag description to homepage view when viewing only one tag

--- tribuna/content/homepage.py
+++ tribuna/content/homepage.py
@@ -1,4 +1,5 @@
 from five import grok
+from plone import api
 from zope.interface import Interface
 
 
@@ -28,3 +29,21 @@ class HomePageView(grok.View):
         if('content_list' in session.keys()):
             return session['content_list']
         return []
+
+    def only_one_tag(self):
+        sdm = self.context.session_data_manager
+        session = sdm.getSessionData(create=True)
+        if('portlet_data' in session.keys()):
+            return len(session['portlet_data']['tags']) == 1
+        return False
+
+    def tag_description(self):
+        sdm = self.context.session_data_manager
+        session = sdm.getSessionData(create=True)
+        title = session['portlet_data']['tags'][0]
+        catalog = api.portal.get_tool(name='portal_catalog')
+        tag = catalog(
+            Title=title,
+            portal_type='tribuna.content.tag',
+        )[0].getObject()
+        return tag.description
