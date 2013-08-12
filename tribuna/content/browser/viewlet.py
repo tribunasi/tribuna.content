503bb68d45a3f41c51bd2c5c6f72ba58149ed2bc
Termitnjak d.o.o <info@termitnjak.si>
2013-08-12 13:35:19 +0000

    move navbar template code to an actual template, simplify the navbar viewlet code

--- tribuna/content/browser/viewlet.py
+++ tribuna/content/browser/viewlet.py
@@ -5,59 +5,30 @@
 
 
 from plone import api
-from Products.CMFPlone.utils import safe_unicode
 from Products.Five.browser import BrowserView
+from plone.app.layout.viewlets import common as base
 from zope.interface import implements
 from zope.viewlet.interfaces import IViewlet
 
-from tribuna.content.entrypage import View
 from tribuna.content.homepage import HomePageView
 from tribuna.content.mainpage import MainPageView
 from tribuna.content.utils import get_articles
 
 
-class NavbarViewlet(BrowserView):
+class NavbarViewlet(base.ViewletBase):
+    """Our custom global navigation viewlet."""
     implements(IViewlet)
 
-    def __init__(self, context, request, view, manager):
-        super(NavbarViewlet, self).__init__(context, request)
-        self.__parent__ = view
-        self.context = context
-        self.request = request
+    def default_page_url(self):
+        portal = api.portal.get()
+        entry_pages = portal["entry-pages"]
+        default_page = entry_pages[entry_pages.getDefaultPage()]
 
-    def update(self):
-        pass
-
-    def available(self):
-        if isinstance(self.__parent__, View):
-            return False
-        return True
-
-    def render(self):
-        if self.available():
-            root = api.portal.get_navigation_root(self.context).absolute_url()
-            portal = api.portal.get()
-            entry_pages = portal["entry-pages"]
-            default_page = entry_pages[entry_pages.getDefaultPage()]
-            return safe_unicode(navbar_template % (
-                root,
-                (root + '/++theme++tribuna.diazotheme/img/home.png'),
-                (root + '/home-page'),
-                (root + '/articles/++add++tribuna.content.article'),
-                (root + '/tags/++add++tribuna.content.tag'),
-                (root + '/images/createObject?type_name=Image'),
-                (default_page.absolute_url() + "/edit"),
-                (root + '/articles'),
-                (root + '/tags'),
-                (root + '/images')
-                )
-            )
-        return ""
+        return default_page.absolute_url()
 
 
 class DefaultSessionViewlet(BrowserView):
-    """Method that checks our session to get
-    the article we want to show first"""
+    """Viewlet for managing the session"""
 
     implements(IViewlet)
 
@@ -95,18 +66,3 @@ class DefaultSessionViewlet(BrowserView):
                 or isinstance(self.__parent__, MainPageView)):
             self.reset_session()
         return ""
-
-
-navbar_template = u"""
-<ul id="navbar-template">
-    <li><a class="brand" href="%s"><img src="%s" alt="Home"/></a></li>
-    <li><a href="%s">Home page</a></li>
-    <li><a href="%s">Add article</a></li>
-    <li><a href="%s">Add tag</a></li>
-    <li><a href="%s">Add image</a></li>
-    <li><a href="%s">Edit entry page</a></li>
-    <li><a href="%s">Manage articles</a></li>
-    <li><a href="%s">Manage tags</a></li>
-    <li><a href="%s">Manage images</a></li>
-</ul>
-"""
