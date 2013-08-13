0256541016eeaad27e2fd03e1408c03ce2098b95
Termitnjak d.o.o <info@termitnjak.si>
2013-08-13 18:16:54 +0000

    move views for rendering comments and content action viewlets into browser/viewlets.py, extract method get_viewlet

--- tribuna/content/article.py
+++ tribuna/content/article.py
@@ -3,18 +3,11 @@
 
 """The Article content type."""
 
-from collective.z3cform.widgets.token_input_widget import TokenInputFieldWidget
 from five import grok
 from plone import api
-from plone.app.layout.globals.interfaces import IViewView
 from plone.directives import form
 from plone.namedfile.field import NamedBlobImage
-from Products.Five.browser import BrowserView
 from zope import schema
-from zope.component import getMultiAdapter
-from zope.interface import alsoProvides
-from zope.interface import Interface
-from zope.viewlet.interfaces import IViewletManager
 
 from tribuna.content import _
 
@@ -35,85 +28,6 @@ class IArticle(form.Schema):
         description=_(u"Will be displayed on the article view."),
         required=False,
     )
-
-
-class CommentsView(grok.View):
-    """View for showing all the comments on the article and adding new
-    comments.
-    """
-
-    grok.context(IArticle)
-    grok.require('zope2.View')
-    grok.name('comments-view')
-
-    def get_comment_viewlet(self):
-        """Method for getting comments viewlet so we can use it in
-        other views"""
-
-        request = self.request
-        context = self.context
-
-        # viewlet managers also require a view object for adaptation
-        view = BrowserView(context, request)
-        if not IViewView.providedBy(view):
-            alsoProvides(view, IViewView)
-
-        # finally, you need the name of the manager you want to find
-        manager_name = 'plone.belowcontent'
-
-        # viewlet managers are found by Multi-Adapter lookup
-        manager = getMultiAdapter(
-            (context, request, view), IViewletManager, manager_name)
-
-        # calling update() on a manager causes it to set up its viewlets
-        manager.update()
-
-        all_viewlets = manager.viewlets
-        for viewlet in all_viewlets:
-            if viewlet.__name__ == u"plone.comments":
-                # Set the widget factory for our field
-                viewlet.form.fields['subject'].widgetFactory['input'] =\
-                    TokenInputFieldWidget
-                # Update widgets so it takes effect
-                viewlet.form.updateWidgets()
-                return viewlet.render()
-
-
-class ContentActionsView(grok.View):
-    """View for showing the content actions. Looks like Plone really needs
-    his context to work properly.
-    """
-
-    grok.context(Interface)
-    grok.require('zope2.View')
-    grok.name('contentactions-view')
-
-    def get_contentactions_viewlet(self):
-        """Method for getting content actions viewlet so we can use it in
-        other views"""
-
-        request = self.request
-        context = self.context
-
-        # viewlet managers also require a view object for adaptation
-        view = BrowserView(context, request)
-        if not IViewView.providedBy(view):
-            alsoProvides(view, IViewView)
-
-        # finally, you need the name of the manager you want to find
-        manager_name = 'plone.contentviews'
-
-        # viewlet managers are found by Multi-Adapter lookup
-        manager = getMultiAdapter(
-            (context, request, view), IViewletManager, manager_name)
-
-        # calling update() on a manager causes it to set up its viewlets
-        manager.update()
-
-        all_viewlets = manager.viewlets
-        for viewlet in all_viewlets:
-            if viewlet.__name__ == u"plone.contentactions":
-                return viewlet.render()
 
 
 class View(grok.View):
--- tribuna/content/browser/viewlet.py
+++ tribuna/content/browser/viewlet.py
@@ -3,16 +3,54 @@
 
 """Viewlets."""
 
-
+from collective.z3cform.widgets.token_input_widget import TokenInputFieldWidget
+from five import grok
 from plone import api
 from Products.Five.browser import BrowserView
 from plone.app.layout.viewlets import common as base
+from plone.app.layout.globals.interfaces import IViewView
 from zope.interface import implements
 from zope.viewlet.interfaces import IViewlet
+from zope.component import getMultiAdapter
+from zope.interface import alsoProvides
+from zope.interface import Interface
+from zope.viewlet.interfaces import IViewletManager
 
 from tribuna.content.homepage import HomePageView
 from tribuna.content.mainpage import MainPageView
 from tribuna.content.utils import get_articles
+
+# set the directory for templates
+grok.templatedir('templates')
+
+
+def get_viewlet(context=None, request=None, manager=None, name=None):
+    """Helper method for getting the viewlet for the provided viewlet name
+    and viewlet manager.
+
+    :param context: context object
+    :param request: request
+    :param manager: viewlet manager, for which the viewlet is registered
+    :param name: viewlet name
+    :returns: viewlet, if found, None otherwise
+    """
+
+    # viewlet managers also require a view object for adaptation
+    view = BrowserView(context, request)
+    if not IViewView.providedBy(view):
+        alsoProvides(view, IViewView)
+
+    # viewlet managers are found by Multi-Adapter lookup
+    manager = getMultiAdapter(
+        (context, request, view), IViewletManager, manager)
+
+    # calling update() on a manager causes it to set up its viewlets
+    manager.update()
+
+    all_viewlets = manager.viewlets
+    for viewlet in all_viewlets:
+        if viewlet.__name__ == name:
+            return viewlet
 
 
 class NavbarViewlet(base.ViewletBase):
@@ -37,9 +75,6 @@ class DefaultSessionViewlet(BrowserView):
         self.__parent__ = view
         self.context = context
         self.request = request
-
-    def update(self):
-        pass
 
     def set_default_content_list(self, session):
         get_articles(session)
@@ -66,3 +101,55 @@ class DefaultSessionViewlet(BrowserView):
                 or isinstance(self.__parent__, MainPageView)):
             self.reset_session()
         return ""
+
+
+class CommentsView(grok.View):
+    """View for rendering the comments viewlet.
+
+    XXX: We need this e.g. on the @@articles view, because we load the content
+    via AJAX and we don't have a proper article context.
+    """
+
+    grok.context(Interface)
+    grok.require('zope2.View')
+    grok.name('comments-view')
+
+    def render_comments(self):
+        """Render the comments viewlet."""
+        viewlet = get_viewlet(
+            context=self.context,
+            request=self.request,
+            manager=u'plone.belowcontent',
+            name=u'plone.comments'
+        )
+        # Set the widget factory for our field
+        viewlet.form.fields['subject'].widgetFactory['input'] =\
+            TokenInputFieldWidget
+
+        # Update widgets so it takes effect
+        viewlet.form.updateWidgets()
+        return viewlet.render()
+
+
+class ContentActionsView(grok.View):
+    """View for rendering the content actions.
+
+    XXX: We need this e.g. on the @@articles view, because we load the content
+    via AJAX and we don't have a proper article context.
+    """
+
+    grok.context(Interface)
+    grok.require('zope2.View')
+    grok.name('contentactions-view')
+
+    def render_content_actions(self):
+        """Render content actions viewlet (dropdown with 'Copy', 'Cut',
+        changing the workflow state etc.).
+        """
+        viewlet = get_viewlet(
+            context=self.context,
+            request=self.request,
+            manager=u'plone.contentviews',
+            name=u'plone.contentactions'
+        )
+        return viewlet.render()
